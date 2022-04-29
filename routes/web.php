0b9fbceeff9482a38bf20a9f3abf3465d949a37d
taoufik ahjam <taoufikahjam@gmail.com>
2022-04-29 22:44:40 +0200

    login and register component and logout section

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,3 +30,24 @@ Route::get('/checkout',CheckoutComponent::class)->name('checkout');
 Route::get('/shop',ShopComponent::class)->name('shop');
 
 
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+Route::middleware(['auth:sanctum','verified'])->group(function(){
+    Route::get('/user/dashboard',UserDashboardComponent::class)->name('user.dashboard');
+    
+});
+
+Route::middleware(['auth:sanctum','verified','authAdmin'])->group(function(){
+    
+    Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+
+});
